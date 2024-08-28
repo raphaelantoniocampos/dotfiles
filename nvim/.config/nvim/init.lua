d6f9dad2b04503fe2218eb37532771b00af2f54e
Raphael Campos <raphaelcampos01@proton.me>
2024-08-28 14:25:23 -0300

    i3 on void

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -229,29 +229,29 @@ require("lazy").setup({
 		end,
 	},
 
-{
-  "allaman/emoji.nvim",
-  version = "1.0.0", -- optionally pin to a tag
-  ft = "markdown", -- adjust to your needs
-  dependencies = {
-    -- optional for nvim-cmp integration
-    "hrsh7th/nvim-cmp",
-    -- optional for telescope integration
-    "nvim-telescope/telescope.nvim",
-  },
-  opts = {
-    -- default is false
-    enable_cmp_integration = false,
-    -- optional if your plugin installation directory
-    -- is not vim.fn.stdpath("data") .. "/lazy/
-    plugin_path = vim.fn.expand("$HOME/plugins/"),
-  },
-  config = function(_, opts)
-    require("emoji").setup(opts)
-    -- optional for telescope integration
-    require("telescope").load_extension("emoji")
-  end,
-},
+	{
+		"allaman/emoji.nvim",
+		version = "1.0.0", -- optionally pin to a tag
+		ft = "markdown", -- adjust to your needs
+		dependencies = {
+			-- optional for nvim-cmp integration
+			"hrsh7th/nvim-cmp",
+			-- optional for telescope integration
+			"nvim-telescope/telescope.nvim",
+		},
+		opts = {
+			-- default is false
+			enable_cmp_integration = false,
+			-- optional if your plugin installation directory
+			-- is not vim.fn.stdpath("data") .. "/lazy/
+			plugin_path = vim.fn.expand("$HOME/plugins/"),
+		},
+		config = function(_, opts)
+			require("emoji").setup(opts)
+			-- optional for telescope integration
+			require("telescope").load_extension("emoji")
+		end,
+	},
 
 	{
 		"stevearc/oil.nvim",
@@ -351,7 +351,7 @@ require("lazy").setup({
 									hidden = true,
 									no_ignore = false,
 									no_ignore_parent = false,
-									file_ignore_patterns = {".git/"},
+									file_ignore_patterns = { ".git/" },
 								})
 							end,
 							key = ".",
@@ -701,9 +701,8 @@ require("lazy").setup({
 				"debugpy",
 				"mypy",
 				"ruff",
-				"nil",
-				"nixpkgs-fmt",
-				"rnix-lsp",
+				"html-lsp",
+				"prettier",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
