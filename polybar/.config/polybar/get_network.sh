65dce3056a1e1989d351faff5a57b2e9a89ea466
Raphael Campos <raphael.antonio.campos@proton.me>
2025-02-01 09:24:54 -0300

    poly get network

--- polybar/.config/polybar/get_network.sh
+++ polybar/.config/polybar/get_network.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Verifica se há uma conexão wireless ativa
-wireless=$(nmcli -t -f ACTIVE,DEVICE,SSID dev wifi | awk -F ':' '$1 == "sim" {print $2 ": " $3}')
+# Verifica se há uma conexão ativa (priorizando a conexão cabeada)
+eth_connection=$(nmcli -t -f DEVICE,STATE | grep -e "conectado" -e "connected" | grep "ethernet")
 
-# Verifica se há uma conexão ethernet ativa
-ethernet=$(nmcli -t -f ACTIVE,DEVICE,STATE | grep -e "conectado" -e "connected" | grep -e "cabeada" -e "" | grep -v "externally" | awk -F: '{print $1 ":" " connected"}')
-
-# Mostra a conexão ativa
-if [ -n "$ethernet" ]; then
-    echo $ethernet
-elif [ -n "$wireless" ]; then
-    echo $wireless
+# Se não encontrar conexão cabeada, verifica a conexão wireless
+if [ -z "$eth_connection" ]; then
+    wifi_connection=$(nmcli -t -f DEVICE,STATE | grep -e "conectado" -e "connected" | grep -v "externally" | grep -v "desconectado" | awk -F: '{print $1}')
+    
+    if [ -n "$wifi_connection" ]; then
+        connection="$wifi_connection"
+    else
+        connection="No Connection"
+    fi
 else
-    echo "No Connection"
+    connection="$eth_connection"
 fi
+
+echo "$connection"
 
