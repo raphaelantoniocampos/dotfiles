731bb04ae38cef40f10cc6cece94a2805afb1710
Raphael Campos <raphael.antonio.campos@proton.me>
2025-03-08 18:13:08 -0300

    waybar zypper volume player

--- bashrc/.bashrc
+++ bashrc/.bashrc
@@ -37,7 +37,6 @@ alias zin="sudo zypper in"
 alias zse="sudo zypper se"
 alias zre="sudo zypper remove"
 alias zclean='sudo zypper packages --unneeded | grep ^i > /tmp/unneeded_packages.txt && if [ -s /tmp/unneeded_packages.txt ]; then cat /tmp/unneeded_packages.txt | cut -d '\''|'\'' -f3 | xargs sudo zypper rm -y --clean-deps; else echo "No unneeded packages found."; fi'
-alias zclean_old='sudo zypper packages --unneeded | grep ^i|cut -d '\''|'\'' -f3|xargs sudo zypper rm -y --clean-deps'
 
 # NOTE:Trash-cli
 alias rm='echo "This is not the command you are looking for, try trp. (If you really want it use \rm)"; false'
--- waybar/.config/waybar/custom_modules/mediaplayer.sh
+++ waybar/.config/waybar/custom_modules/mediaplayer.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
 player_status=$(playerctl status 2> /dev/null)
-icon="<span color='#89dceb' >󰎇</span>"
+
 if [ "$player_status" = "Playing" ]; then
-    echo "${icon}  $(playerctl metadata artist) - $(playerctl metadata title)"
+    icon="<span color='#94e2d5'></span>"
 elif [ "$player_status" = "Paused" ]; then
+    icon="<span color='#f38ba8'></span>"
+else
+    icon=""
+fi
+
+# Exibe o artista e o título se o player estiver tocando ou pausado
+if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]; then
     echo "${icon}  $(playerctl metadata artist) - $(playerctl metadata title)"
 else
-    # echo "${icon}  Nothing Playing yet."
     echo ""
 fi
