2797ca3936c26d5635aed18c714adcd3321250e3
raphaelac <raphaelac@localhost.localdomain>
2025-05-21 12:01:03 -0300

    x copy

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -33,7 +33,7 @@ vim.keymap.set('n', '<leader>cF', function()
 end, opts 'Format')
 
 -- delete single character without copying into register
-vim.keymap.set('n', 'x', '"_x', opts())
+-- vim.keymap.set('n', 'x', '"_x', opts())
 
 -- Vertical scroll and center
 vim.keymap.set('n', '<C-d>', '<C-d>zz', opts())
