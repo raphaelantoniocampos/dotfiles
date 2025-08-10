24d869e908b2baace084a6d6875ec86b6ec0c008
Raphael Campos <raphael.antonio.campos@proton.me>
2025-08-10 14:04:36 -0300

    update battery notify script

--- scripts/battery-notify.sh
+++ scripts/battery-notify.sh
@@ -2,15 +2,23 @@
 while true; do
   bat_lvl=$(cat /sys/class/power_supply/BAT1/capacity)
   bat_status=$(cat /sys/class/power_supply/BAT1/status)
+  brightness=$(brightnessctl get)
 
-  if [ "$bat_status" = "Charging" ] || [ "$bat_status" = "Full" ]; then
-    sleep 300
+  if [ "$bat_status" = "Charging" ]; then
+    if [ "$brightness" >= 240 ]; then
+      hyprctl notify 5 3000 0 "Battery Charging - Level: ${bat_lvl}%"
+      brightnessctl set 100%
+    fi
+    sleep 600
     continue
+
+  elif [ "$bat_status" = "Full" ]; then
+    hyprctl notify 5 3000 0 "Battery Full - Level: ${bat_lvl}%"
+    sleep 600
 
   elif [ "$bat_lvl" -le 5 ]; then
     hyprctl notify 3 10000 0 "⚠ CRITICAL Battery - Level: ${bat_lvl}%"
 
-    brightness=$(brightnessctl get)
     if [ "$brightness" != 240 ]; then
       brightnessctl set 30%
     fi
