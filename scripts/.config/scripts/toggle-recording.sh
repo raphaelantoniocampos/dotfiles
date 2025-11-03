be0be161925fd5eb11d98db615fa125baedeb187
Raphael Campos <raphael.antonio.campos@proton.me>
2025-11-03 20:47:00 -0300

    recording script updated

--- scripts/.config/scripts/toggle-recording.sh
+++ scripts/.config/scripts/toggle-recording.sh
@@ -3,15 +3,16 @@
 RECORDING_DIR="$HOME/Videos/recordings"
 DATE_FORMAT=$(date +%Y-%m-%d_%H-%M-%S)
 NOTIFY_TIMEOUT=3000 # 3 seconds
+DEVICE_NAME=$(pactl list sources | grep 'Name:' | head -n 1 | awk '{print $2}')
 
 if pgrep -x "wf-recorder" > /dev/null; then
     pkill -INT -x wf-recorder
     
     hyprctl notify 5 $NOTIFY_TIMEOUT 0 "Stopped Recording"
 else
-    wf-recorder -f "$RECORDING_DIR/$DATE_FORMAT.mkv" & 
+    wf-recorder -a="$DEVICE_NAME" -f "$RECORDING_DIR/$DATE_FORMAT.mp4" & 
     
-    hyprctl notify 1 $NOTIFY_TIMEOUT 0 "Recording Started: $DATE_FORMAT.mkv"
+    hyprctl notify 1 $NOTIFY_TIMEOUT 0 "Recording Started: $DATE_FORMAT.mp4"
 fi
 
 exit 0
