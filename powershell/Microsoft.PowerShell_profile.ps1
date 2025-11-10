dec40d93bff9a156d446b552dd7689a398529c48
Raphael Campos <raphael.antonio.campos@proton.me>
2025-11-10 16:19:35 -0300

    Powershell: add

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,22 @@
+# cmd link
+# mklink "D:\Perfil\%USERNAME%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\%USERNAME%\dotfiles\powershell\Microsoft.PowerShell_profile.ps1"
+
+function prompt()
+{
+    if($_last = Get-History | Select-Object -Last 1)
+    {
+        "$($_last.Duration.ToString())> "
+    }
+}
+$ENV:STARSHIP_CONFIG = "C:\Users\raphael.campos\dotfiles\starship\.config\starship.toml"
+$ENV:LEDGER_FILE = "C:\Users\raphael.campos\obsidian\finance\hledger.journal"
+
+Invoke-Expression (&starship init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+Set-Alias ls lsd
+Set-Alias cat bat
+Set-Alias trp Remove-ItemSafely
+
+fastfetch
+
