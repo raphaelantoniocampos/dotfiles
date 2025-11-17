f9cf4f2cd1aaef201881b2afed21975d305186f0
Raphael Campos <raphaelcampos01@proton.me>
2025-11-16 22:27:33 -0300

    powershell: update powershell link cmd

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,4 @@
-# cmd link
-# mklink "D:\Perfil\%USERNAME%\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\%USERNAME%\dotfiles\powershell\Microsoft.PowerShell_profile.ps1"
+# cmd /c mklink "C:\Users\$ENV:USERNAME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "C:\Users\$ENV:USERNAME\dotfiles\powershell\Microsoft.PowerShell_profile.ps1"
 
 function prompt()
 {
@@ -8,8 +7,8 @@ function prompt()
         "$($_last.Duration.ToString())> "
     }
 }
-$ENV:STARSHIP_CONFIG = "C:\Users\raphael.campos\dotfiles\starship\.config\starship.toml"
-$ENV:LEDGER_FILE = "C:\Users\raphael.campos\obsidian\finance\hledger.journal"
+$ENV:STARSHIP_CONFIG = "C:\Users\$ENV:USERNAME\dotfiles\starship\.config\starship.toml"
+$ENV:LEDGER_FILE = "C:\Users\$ENV:USERNAME\obsidian\finance\hledger.journal"
 
 Invoke-Expression (&starship init powershell)
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
