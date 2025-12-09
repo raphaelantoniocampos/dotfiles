1408e6460806813a34fa856f5fddefe292c562c8
Raphael Campos <raphael.antonio.campos@proton.me>
2025-12-09 18:42:17 -0300

    scripts: update time for recording

--- scripts/.config/scripts/toggle-recording.sh
+++ scripts/.config/scripts/toggle-recording.sh
@@ -2,7 +2,7 @@
 
 RECORDING_DIR="$HOME/Videos/recordings"
 DATE_FORMAT=$(date +%Y-%m-%d_%H-%M-%S)
-NOTIFY_TIMEOUT=3000 # 3 seconds
+NOTIFY_TIMEOUT=2000
 DEVICE_NAME=$(pactl list sources | grep 'Name:' | head -n 1 | awk '{print $2}')
 
 if pgrep -x "wf-recorder" > /dev/null; then
@@ -10,9 +10,12 @@ if pgrep -x "wf-recorder" > /dev/null; then
     
     hyprctl notify 5 $NOTIFY_TIMEOUT 0 "Stopped Recording"
 else
+    hyprctl notify 1 $NOTIFY_TIMEOUT 0 "Recording Starting in 2.0 seconds..."
+
+    sleep 2
+
     wf-recorder -a="$DEVICE_NAME" -f "$RECORDING_DIR/$DATE_FORMAT.mp4" & 
     
-    hyprctl notify 1 $NOTIFY_TIMEOUT 0 "Recording Started: $DATE_FORMAT.mp4"
 fi
 
 exit 0
