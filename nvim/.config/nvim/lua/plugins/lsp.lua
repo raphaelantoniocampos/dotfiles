6bf14cb07e26c112491d45bcae099525ebaf44c4
Raphael Campos <raphael.antonio.campos@proton.me>
2025-12-28 18:00:31 -0300

    nvim: rm pylsp add pyrefly

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -133,50 +133,50 @@ return { -- LSP Configuration & Plugins
 					},
 				},
 			},
-			pylsp = {
-				settings = {
-					pylsp = {
-						plugins = {
-							pyflakes = { enabled = false },
-							pycodestyle = { enabled = false },
-							autopep8 = { enabled = false },
-							yapf = { enabled = false },
-							mccabe = { enabled = false },
-							pylsp_mypy = { enabled = false },
-							pylsp_black = { enabled = false },
-							pylsp_isort = { enabled = false },
-						},
-					},
-				},
-			},
-			-- ruff = {
-			--   -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
-			--   -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
-			--   commands = {
-			--     RuffAutofix = {
-			--       function()
-			--         vim.lsp.buf.execute_command {
-			--           command = 'ruff.applyAutofix',
-			--           arguments = {
-			--             { uri = vim.uri_from_bufnr(0) },
-			--           },
-			--         }
-			--       end,
-			--       description = 'Ruff: Fix all auto-fixable problems',
-			--     },
-			--     RuffOrganizeImports = {
-			--       function()
-			--         vim.lsp.buf.execute_command {
-			--           command = 'ruff.applyOrganizeImports',
-			--           arguments = {
-			--             { uri = vim.uri_from_bufnr(0) },
-			--           },
-			--         }
-			--       end,
-			--       description = 'Ruff: Format imports',
-			--     },
-			--   },
+			-- pylsp = {
+			-- 	settings = {
+			-- 		pylsp = {
+			-- 			plugins = {
+			-- 				pyflakes = { enabled = false },
+			-- 				pycodestyle = { enabled = false },
+			-- 				autopep8 = { enabled = false },
+			-- 				yapf = { enabled = false },
+			-- 				mccabe = { enabled = false },
+			-- 				pylsp_mypy = { enabled = false },
+			-- 				pylsp_black = { enabled = false },
+			-- 				pylsp_isort = { enabled = false },
+			-- 			},
+			-- 		},
+			-- 	},
 			-- },
+			ruff = {
+			  -- Notes on code actions: https://github.com/astral-sh/ruff-lsp/issues/119#issuecomment-1595628355
+			  -- Get isort like behavior: https://github.com/astral-sh/ruff/issues/8926#issuecomment-1834048218
+			  commands = {
+			    RuffAutofix = {
+			      function()
+			        vim.lsp.buf.execute_command {
+			          command = 'ruff.applyAutofix',
+			          arguments = {
+			            { uri = vim.uri_from_bufnr(0) },
+			          },
+			        }
+			      end,
+			      description = 'Ruff: Fix all auto-fixable problems',
+			    },
+			    RuffOrganizeImports = {
+			      function()
+			        vim.lsp.buf.execute_command {
+			          command = 'ruff.applyOrganizeImports',
+			          arguments = {
+			            { uri = vim.uri_from_bufnr(0) },
+			          },
+			        }
+			      end,
+			      description = 'Ruff: Format imports',
+			    },
+			  },
+			},
 			rust_analyzer = {
 				["rust-analyzer"] = {
 					cargo = {
