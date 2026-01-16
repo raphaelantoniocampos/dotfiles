3e331ef5b6239e79aaf7b9e6954b3c0999ab14d4
Raphael Campos <raphael.antonio.campos@proton.me>
2026-01-15 23:32:48 -0300

    i3: added i3 and x11 apps

--- polybar/.config/polybar/last_nixos_gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# current_generation.sh
-
-# Obtenha a geração atual
-current_gen=$(nixos-rebuild list-generations | awk '/current/ {print $1}')
-
-# Exiba a geração atual
-echo $current_gen
