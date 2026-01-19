89867039594eba7f235c796a7bd0d7f56a8c1588
Raphael Campos <raphael.antonio.campos@proton.me>
2026-01-18 23:27:14 -0300

    scripts: fix battery-notify script

--- scripts/.config/scripts/battery-notify.sh
+++ scripts/.config/scripts/battery-notify.sh
@@ -3,9 +3,8 @@
 BAT_PATH="/sys/class/power_supply/BAT1"
 
 # Sounds
-SND_CHARGE="/usr/share/sounds/freedesktop/stereo/dialog-information.oga"
+SND_CHANGE="/usr/share/sounds/freedesktop/stereo/dialog-information.oga"
 SND_FULL="/usr/share/sounds/freedesktop/stereo/complete.oga"
-SND_LOW="/usr/share/sounds/freedesktop/stereo/bell.oga"
 SND_CRITICAL="/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga"
 
 last_status=""
@@ -20,29 +19,42 @@ while true; do
   if [[ "$bat_lvl" != "$last_lvl" || "$bat_status" != "$last_status" ]]; then
     case "$bat_status" in
       "Charging")
-        if [ "$last_status" != "$bat_status" ]; then
-          notify-send -u low "Charging - ${bat_lvl}%"
-          pw-play "$SND_CHARGE" >/dev/null 2>&1 &
-          brightnessctl set 100%
-        fi
+          if [ "$bat_status" != "$last_status" ]; then
+            notify-send -u low "Charging - ${bat_lvl}%"
+            pw-play "$SND_CHANGE" >/dev/null 2>&1 &
+            brightnessctl set 100%
+            last_notify=$bat_status
+          fi
         ;;
       "Full")
         notify-send -u low "Battery Full - ${bat_lvl}%"
         pw-play "$SND_FULL" >/dev/null 2>&1 &
+        last_notify=$bat_status
         ;;
       "Discharging")
         if [ "$bat_lvl" -le 5 ]; then
-          notify-send -u critical "CRITICAL - ${bat_lvl}%"
-          pw-play "$SND_CRITICAL" >/dev/null 2>&1 &
-          [ "$brightness" -gt 240 ] && brightnessctl set 30%
+          if [ "$last_notify" != "$bat_status" ]; then
+            notify-send -u critical "CRITICAL - ${bat_lvl}%"
+            pw-play "$SND_CRITICAL" >/dev/null 2>&1 &
+            [ "$brightness" -gt 240 ] && brightnessctl set 30%
+            last_notify=$bat_status
+          fi
         elif [ "$bat_lvl" -le 15 ]; then
-          notify-send -u normal "Battery Low - ${bat_lvl}%" "$SND_LOW" "low"
-          pw-play "$SND_LOW" >/dev/null 2>&1 &
+          if [ "$last_notify" != "$bat_status" ]; then
+            notify-send -u normal "Battery Low - ${bat_lvl}%"
+            pw-play "$SND_CHANGE" >/dev/null 2>&1 &
+            last_notify=$bat_status
+          fi
         fi
         ;;
     esac
   fi
 
+  echo "last_lvl: ${last_lvl}"
+  echo "bat_lvl: ${bat_lvl}"
+  echo "last_status: ${last_status}"
+  echo "bat_status: ${bat_status}"
+  echo "last_notify: ${last_notify}"
   last_lvl=$bat_lvl
   last_status=$bat_status
 
