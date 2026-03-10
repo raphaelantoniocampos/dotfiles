1a19386e15a58fc5b7da2adc647bd2feabafff9f
Raphael Campos <raphael.antonio.campos@proton.me>
2026-03-10 17:54:40 -0300

    nvim: update conform clang format

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,16 +2,15 @@ return { -- Autoformat
   'stevearc/conform.nvim',
   opts = {
     notify_on_error = true,
-    -- format_on_save = {
-    --   timeout_ms = 500,
-    --   lsp_fallback = true,
-    -- },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
     formatters_by_ft = {
       lua = { 'stylua' },
       -- Conform can also run multiple formatters sequentially
       python = { 'ruff' },
-      c = { 'clangd' },
-      gleam = { 'gleam' },
+      c = { 'clang-format' },
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
