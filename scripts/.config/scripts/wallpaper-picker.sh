6adce34bc0752f0ef1edabd047dce2b24e8653c5
Raphael Campos <raphael.antonio.campos@proton.me>
2026-05-18 21:52:55 -0300

    matugen: updating theme config

--- scripts/.config/scripts/wallpaper-picker.sh
+++ scripts/.config/scripts/wallpaper-picker.sh
@@ -2,13 +2,23 @@
 
 WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
 
-SELECTED=$(imv -n 1 "$WALLPAPER_DIR")
-
-if [ -n "$SELECTED" ]; then
-    IMAGE=$(echo "$SELECTED" | tail -n 1)
+if [[ "$1" == "random" ]]; then
+    IMAGE=$(find "$WALLPAPER_DIR" | shuf -n 1)
     
-    matugen image "$IMAGE" --prefer saturation
-    notify-send "Changed Wallpaper" "Themes successfully updated."
+    if [ -z "$IMAGE" ]; then
+        notify-send "Erro" "Nenhum wallpaper encontrado no diretório."
+        exit 1
+    fi
 else
-    notify-send "Selection Canceled" "No wallpaper was selected."
+    SELECTED=$(imv -n 1 "$WALLPAPER_DIR")
+
+    if [ -n "$SELECTED" ]; then
+        IMAGE=$(echo "$SELECTED" | tail -n 1)
+    else
+        notify-send "Selection Canceled" "No wallpaper was selected."
+        exit 0
+    fi
 fi
+
+matugen image "$IMAGE" --prefer value
+notify-send "Changed Wallpaper" "Themes successfully updated para: $(basename "$IMAGE")"
